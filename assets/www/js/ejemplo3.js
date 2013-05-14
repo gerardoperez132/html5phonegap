746c110dcb77c3da7182058a964e7c518e324c24
gerardoperez132 <gerardo.perez132@gmail.com>
2013-05-13 22:46:55 -0430

    Ejemplo 3

--- assets/www/js/ejemplo3.js
+++ assets/www/js/ejemplo3.js
@@ -1,7 +1,66 @@
-@-webkit-keyframes animate-bot{from{margin-top:-120px;} 50%{margin-top:-110px;} to{margin-top:-120px;}}@-moz-keyframes animate-bot{from{margin-top:-120px;} 50%{margin-top:-110px;} to{margin-top:-120px;}}@-ms-keyframes animate-bot{from{margin-top:-120px;} 50%{margin-top:-110px;} to{margin-top:-120px;}}@-o-keyframes animate-bot{from{margin-top:-120px;} 50%{margin-top:-110px;} to{margin-top:-120px;}}.animate-bot-key from{margin-top:-120px;}
-.animate-bot-key 50%{margin-top:-110px;}
-.animate-bot-key to{margin-top:-120px;}
-.prueba {
--webkit-animation:animate-bot 3s ease-in-out infinite;-moz-animation:animate-bot 3s ease-in-out infinite;-ms-animation:animate-bot 3s ease-in-out infinite;-o-animation:animate-bot 3s ease-in-out infinite;animation:animate-bot 3s ease-in-out infinite;
+// forked from about_hiroppy's "particle" http://jsdo.it/about_hiroppy/qHr0
+var canvas;
+var ctx;
+var particles = [];
+var mouseX = 250;
+var mouseY = 250;
+var radian = 100;
+var distance = 50;
+var spring = 0.6;
+var friction = 0.5;
+
+function init(){
+	canvas = document.getElementById('canvas');
+	ctx = canvas.getContext('2d');
+	for(var i=0;i<100;i++){
+		for(var j=0;j<100;j++){
+			var p = {};
+			p.x = i*3+100;
+			p.y = j*3+100;
+			p.tx = i*3+100;
+			p.ty = j*3+100;
+			p.vx = 0;
+			p.vy = 0;
+			particles.push(p);
+		}
+	}
+
+	canvas.onmousemove = onMouseEvent;
+	draw();
 }
+
+function onMouseEvent(e){
+	mouseX = e.clientX;
+    mouseY = e.clientY;
+}
+
+function draw(){
+	ctx.fillStyle = "rgb(0, 0, 0)";
+	ctx.fillRect(0,0,500,500);
+	ctx.fillStyle = "rgb(0,250,250)";
+
+	var n = particles.length;
+	var gx = mouseX;
+	var gy = mouseY;
+
+	while(n--){
+		var p = particles[n];
+		var angle = Math.atan2(p.y - mouseY,p.x - mouseX);
+		var d = Math.sqrt(Math.pow(mouseX-p.x,2)+Math.pow(mouseY-p.y,2));
+		var circle = radian / distance;
+		if(d < distance){
+			p.x += circle*Math.cos(angle) + (p.tx - p.x)*0.1;
+			p.y += circle*Math.sin(angle) + (p.ty - p.y)*0.1;
+		}
+		else{
+			p.vx += (p.tx - p.x)*spring*friction;
+			p.vy += (p.ty - p.y)*spring*friction;
+			p.x += (p.tx - p.x)*0.2+p.vx;
+			p.y += (p.ty - p.y)*0.2+p.vy;
+		}
+		ctx.fillRect(p.x,p.y,1,1);
+	}
+	setTimeout(draw,30);
+}
+
 
